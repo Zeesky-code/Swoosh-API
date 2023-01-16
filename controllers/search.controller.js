133814b16c6b4027a4bef82f40c009a40af4dde9
Zainab Lawal <zainablaw1012@gmail.com>
2023-01-16 13:35:26 +0300

    Adding different options for each music source

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -6,6 +6,21 @@ async function getSpotifyLink(req,res){
     const url = req.body.url;
     let title = await getTitle(url);
     title = title.replace(/[\u{0080}-\u{FFFF}]/gu,"");
+    switch (req.body.source){
+        case "YouTube Music":
+            title = title.replace("- YouTube Music", "")
+            title = title.replace("Taraycnz kullanmdan kaldrlm. Ltfen yeni srme gein.", "")
+            break;
+        case "Deezer":
+            title = title.replace(": listen with lyrics | Deezer","")
+            break;
+        case "Apple Music":
+            title = title.replace("on AppleMusic", "")
+            break;
+        default:
+            title = title;
+    }
+    
     res.status(200).json({
         "status": true,
         "title": title
@@ -21,7 +36,9 @@ async function getTitle(url) {
     // Queue just one URL, with default callback
         c.direct({
             uri: url,
-            skipEventRequest: false, // default to true, direct requests won't trigger Event:'request'
+            forceUTF8: false,
+            skipEventRequest: false,
+             // default to true, direct requests won't trigger Event:'request'
             callback: (error, response, done) => {
                 if (error) {
                     reject(error)
